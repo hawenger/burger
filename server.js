8c695807650b5e83931c3263a7d0b26d2e573a15
Ameco <hamecow@gmail.com>
2020-10-14 17:45:51 -0700

    Add Burger Appearance function - bug where names won't appear and js buttons don't work

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-//Dependencies
 const express = require('express');
-const exphbs = require("express-handlebars");
-
-const app = express();
 
 const PORT = process.env.PORT || 8666;
+
+const app = express();
 
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
