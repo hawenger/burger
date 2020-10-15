b0cb50f60388ea916df70668fcf2df1bd1adaf20
Ameco <hamecow@gmail.com>
2020-10-14 17:00:59 -0700

    Allow server to connect to all data

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 //Dependencies
-const orm = require("./config/orm.js");
 const express = require('express');
 const exphbs = require("express-handlebars");
 
@@ -7,46 +6,20 @@ const app = express();
 
 const PORT = process.env.PORT || 8666;
 
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var lunches = [{
-    lunch: "Beet & Goat Cheese Salad with minestrone soup."
-}, {
-    lunch: "Pizza, two double veggie burgers, fries with a Big Gulp"
-}];
+const routes = require("./controllers/burgers_controller.js");
 
-// Routes
-app.get("/weekday", function(req, res) {
-    res.render("index", lunches[0]);
-});
-
-app.get("/weekend", function(req, res) {
-    res.render("index", lunches[1]);
-});
-
-app.get("/", function(req, res) {
-    res.render("all-burgers", {
-        foods: lunches,
-        eater: "david"
-    });
-});
-
+app.use(routes);
 
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
     console.log("Server listening on: http://localhost:" + PORT);
 });
-
-
-//Express App
-//
-//app.use(express.static(__dirname + '/public'));
-//app.use(express.static(__dirname + '/db'));
-//
-////Listen
-//app.listen(PORT, function() {
-//    console.log('App listening on PORT ' + PORT);
-//});
